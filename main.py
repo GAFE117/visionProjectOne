b9b5757a70f4f702621d1b88e829c17b302b62ee
ernstmv <ernestoroque777@gmail.com>
2024-02-26 23:20:50 -0600

    Segundo reto completado

--- main.py
+++ main.py
@@ -10,19 +10,10 @@ def clear():
 
 class Main:
 
-    num1 = int
-    num2 = int
-    rslt = int
+    users = {}
 
-    u_fist_name = str
-    u_last_name = str
-    u_salarary = float
-
-    def set_num1(self, num1):
-        self.num1 = num1
-
-    def set_num2(self, num2):
-        self.num2 = num2
+    def set_nums(self, num1, num2):
+        self.num1, self.num2 = num1, num2
 
     def operations(self, op):
         if op == 1:
@@ -46,18 +37,84 @@ class Main:
             num2 = input('Ingrese el segundo numero:\n')
             op = input('Seleccione una operacion:\n1) +\n2) -\n3) *\n4) /\n')
             try:
-                self.set_num1(int(num1))
-                self.set_num2(int(num2))
+                self.set_nums(float(num1), float(num2))
                 op = int(op)
                 print(f'\nEl resultado es: {self.operations(op)}')
             except Exception as e:
                 print(f"FATAL ERROR: {e}")
                 continue
 
+    def add_user(self):
+        clear()
+        name = input('Ingrese el nombre del nuevo usuario:\n')
+        self.users[name] = None
+        print('Usuario agregado')
+        sel = input('Dar de alta su salario:\n1) Si\n2)No\n')
+        try:
+            sel = int(sel)
+            if sel == 1:
+                self.set_user_salary(name)
+            else:
+                return None
+        except Exception as e:
+            print(f"FATAL ERROR: {e}")
+
+    def set_user_salary(self, name=None):
+        clear()
+        if name is None:
+            print(list(self.users.keys()))
+            name = input('Ingrese el nombre del usuario:\n')
+        if name not in self.users:
+            print('ERROR: Usuario no encontrado')
+            return None
+        print(f'El salario actual de {name} es {self.users[name]}')
+        salary = input(f'Ingrese el salario de {name} en pesos:\n')
+        self.users[name] = salary
+        print('Salario registrado')
+        input('Presione enter para continuar')
+
+    def get_user_salary(self):
+        clear()
+        print(list(self.users.keys()))
+        name = input('Por favor ingrese el nombre del usuario:\n')
+        if name not in self.users:
+            print('Usuario no encontrado')
+            return None
+        print(self.users[name])
+        input('Presione enter para continuar')
+
+    def action(self, selection):
+        if selection == 1:
+            self.add_user()
+        elif selection == 2:
+            self.get_user_salary()
+        elif selection == 3:
+            self.set_user_salary()
+        else:
+            print("Opcion no disponible")
+
+    def users_menu(self):
+        while True:
+            clear()
+            print('\tREGISTRATHOR')
+            print('Que desea hacer:')
+            print('1) Agregar un nuevo usuario')
+            print('2) Consultar el salario de un usuario')
+            print('3) Modificar el salario de un usuario')
+            print('4) Salir')
+            selection = input()
+            try:
+                selection = int(selection)
+                if selection == 4:
+                    return None
+                self.action(selection)
+            except Exception as e:
+                print(f'FATAL ERROR 01: {e}')
+
 
 def main():
     current = Main()
-    current.calc_menu()
+    current.users_menu()
 
 
 if __name__ == '__main__':
